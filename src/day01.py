0fa94602ac41a6c4a2b6d93299e6154c4fd45a1d
Valentin Berlier <berlier.v@gmail.com>
2023-12-02 11:16:34 +0100

    Redundant .read().splitlines()

--- src/day01.py
+++ src/day01.py
@@ -1,2 +1,2 @@
-print(sum((x:=[int(d)for d in l if d.isdigit()])[0]*10+x[-1]for l in open('day01.txt').read().splitlines()))
-print(sum((x:=[int(d)for(i,c)in enumerate(l)if(d:=c).isdigit()or(d:=next((j+1 for(j,n)in enumerate('one two three four five six seven eight nine'.split())if l.startswith(n,i)),0))])[0]*10+x[-1]for l in open('day01.txt').read().splitlines()))
+print(sum((x:=[int(d)for d in l if d.isdigit()])[0]*10+x[-1]for l in open('day01.txt')))
+print(sum((x:=[int(d)for(i,c)in enumerate(l)if(d:=c).isdigit()or(d:=next((j+1 for(j,n)in enumerate('one two three four five six seven eight nine'.split())if l.startswith(n,i)),0))])[0]*10+x[-1]for l in open('day01.txt')))
